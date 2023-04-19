82f5275166c343c10accda5266c8f337b46d62f3
lallo214 <lallo.pb@gmail.com>
2023-04-18 21:35:47 -0300

    primeira questao da lista

---
+++ PrimeiraQuestao.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PrimeiraQuestao{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("qual seu nome?");
+        String nome = scanner.nextLine();
+        System.out.println("qual sua idade");
+        int idade = scanner.nextInt();
+        System.out.println("ola " + nome + " sua idade é "+ idade);
+        
+    }
+}
